682d7e87dd81678fe98c94e6a825f590436b6a32
dillon <dillon.chen1@gmail.com>
2023-11-22 13:01:00 +0100

    new ILG

--- learner/scripts_kernel/collect_gadi_test.sh
+++ learner/scripts_kernel/collect_gadi_test.sh
@@ -1,6 +1,4 @@
-mkdir -p logs/opt_ipc2023_test
-ssh gadi "zip -r opt_ipc2023_test.zip goose/learner/logs_kernel/opt_ipc2023_test"
-scp gadi:~/opt_ipc2023_test.zip .
-unzip opt_ipc2023_test
-mv goose/learner/logs_kernel/opt_ipc2023_test/* logs/opt_ipc2023_test
-rm -rf goose/learner/logs_kernel/opt_ipc2023_test/
+mkdir -p icaps24_logs
+ssh gadi "cd /scratch/xb83/dc6693/goose-kernels/learner/; zip -r icaps24_logs.zip icaps24_logs"
+scp gadi:/scratch/xb83/dc6693/goose-kernels/learner/icaps24_logs.zip .
+unzip icaps24_logs.zip
