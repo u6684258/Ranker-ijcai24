5170e19d1b3c418a9e18defbcb9a86edb027b72b
dillon <dillon.chen1@gmail.com>
2023-11-23 14:15:31 +0100

    backup trained models

--- learner/scripts_kernel/train_all.py
+++ learner/scripts_kernel/train_all.py
@@ -1,31 +1,52 @@
-import os 
+import os
 from itertools import product
 from tqdm import tqdm
 
-SAVE_DIR = "trained_kernel_models"
-LOG_DIR = "logs_training_kernels"
+SAVE_DIR = "icaps24_wl_models"
+LOG_DIR = "icaps24_train_logs"
 
 os.makedirs(LOG_DIR, exist_ok=True)
 
 configs = product(
-    ["wl"],  # wl algorithms
-    [1,2,3,4,5],  # iterations
-    ["ig"],  # representation
-    ["ferry", "blocksworld", "childsnack", "floortile", "miconic", "rovers", "satellite", "sokoban", "spanner", "transport"],  # domains
-    ["linear-svr", "lasso", "ridge", "rbf-svr", "quadratic-svr", "cubic-svr", "mlp"],  # models
+    ["1wl", "2gwl", "2lwl"],  # wl algorithms
+    [1],  # iterations
+    [0],  # prunes
+    ["ilg"],  # representation
+    [  # domains
+        "blocksworld",
+        "childsnack",
+        "ferry",
+        "floortile",
+        "miconic",
+        "rovers",
+        "satellite",
+        "sokoban",
+        "spanner",
+        "transport",
+    ],
+    [  # models
+        "linear-svr",
+        # "lasso",
+        # "ridge",
+        # "rbf-svr",
+        # "quadratic-svr",
+        # "cubic-svr",
+        # "mlp",
+    ],
 )
 
-# deadend_configs = [("H", ""), ("D", "--deadends")]
-deadend_configs = [("D", "--deadends")]
+# deadend_configs = [("H", "?"), ("D", "--deadends")]
+# deadend_configs = [("D", "--deadends")]
+deadend_configs = [("H", "")]
 
-for wl, iterations, rep, domain, model in tqdm(configs):
-    for target, flag in deadend_configs:
-        desc = f"{wl}_{iterations}_{rep}_{model}_{target}_{domain}"
+for k, l, p, r, domain, m in tqdm(list(configs)):
+    for t, flag in deadend_configs:
+        desc = f"{domain}_{r}_{k}_{l}_{p}_{m}_{t}"
         save_file = f"{SAVE_DIR}/{desc}.joblib"
         log_file = f"{LOG_DIR}/{desc}.log"
         if os.path.exists(save_file) and os.path.exists(log_file):
             continue
-        cmd = f"python3 train_kernel.py {domain} {flag} -k {wl} -l {iterations} -r {rep} -m {model} --save-file {save_file}"
+        cmd = f"python3 train_kernel.py -d {domain} -k {k} -l {l} -r {r} -m {m} {flag} --model-save-file {save_file}"
         cmd = f"{cmd} > {log_file}"
         print(cmd)
         os.system(cmd)
