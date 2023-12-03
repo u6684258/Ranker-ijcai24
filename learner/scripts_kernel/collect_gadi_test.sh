ab33899f622d143d60b468b2f647e727d99be02f
dillon <dillon.chen1@gmail.com>
2023-12-03 11:10:50 +0100

    mq: 492; hff: 430

--- learner/scripts_kernel/collect_gadi_test.sh
+++ learner/scripts_kernel/collect_gadi_test.sh
@@ -1,6 +1,9 @@
 mkdir -p icaps24_test_logs
 
-ssh gadi "cd /scratch/xb83/dc6693/goose-kernels/learner/; zip -r icaps24_test_logs.zip icaps24_test_logs"
+echo "collecting combined model logs..."
+ssh gadi "cd /scratch/xb83/dc6693/goose-kernels/learner/; zip -r icaps24_test_logs.zip icaps24_test_logs/*combined*"
+
+# ssh gadi "cd /scratch/xb83/dc6693/goose-kernels/learner/; zip -r icaps24_test_logs.zip icaps24_test_logs/"
 
 scp gadi:/scratch/xb83/dc6693/goose-kernels/learner/icaps24_test_logs.zip .
 
