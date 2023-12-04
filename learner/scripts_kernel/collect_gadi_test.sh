fee6dc5bcbdb49c8152598f3ff7aaf49ada4ba3b
dillon <dillon.chen1@gmail.com>
2023-12-04 17:01:00 +0100

    backup scripts

--- learner/scripts_kernel/collect_gadi_test.sh
+++ learner/scripts_kernel/collect_gadi_test.sh
@@ -6,10 +6,11 @@ mkdir -p icaps24_test_logs
 # echo "collecting gpr logs..."
 # ssh gadi "cd /scratch/xb83/dc6693/goose-kernels/learner/; zip -r icaps24_test_logs.zip icaps24_test_logs/*gp*"
 
-echo "collecting combined gpr logs..."
-ssh gadi "cd /scratch/xb83/dc6693/goose-kernels/learner/; zip -r icaps24_test_logs.zip icaps24_test_logs/*combined_gp*"
+# echo "collecting combined gpr logs..."
+# ssh gadi "cd /scratch/xb83/dc6693/goose-kernels/learner/; zip -r icaps24_test_logs.zip icaps24_test_logs/*combined_gp*"
 
-# ssh gadi "cd /scratch/xb83/dc6693/goose-kernels/learner/; zip -r icaps24_test_logs.zip icaps24_test_logs/"
+echo "collecting MIP logs..."
+ssh gadi "cd /scratch/xb83/dc6693/goose-kernels/learner/; zip -r icaps24_test_logs.zip icaps24_test_logs/*mip*"
 
 scp gadi:/scratch/xb83/dc6693/goose-kernels/learner/icaps24_test_logs.zip .
 
--- learner/scripts_kernel/scp_models.sh
+++ learner/scripts_kernel/scp_models.sh
@@ -1,3 +1,4 @@
 mkdir -p icaps24_wl_models
 cd icaps24_wl_models
 scp * gadi:/scratch/xb83/dc6693/goose-kernels/learner/icaps24_wl_models
+
