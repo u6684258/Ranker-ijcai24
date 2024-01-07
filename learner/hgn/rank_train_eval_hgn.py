06a01778a36f3c4a6220a85ec2347205990fa9ff
Mingyu Hao <mingyu.hao@anu.edu.au>
2024-01-07 19:44:50 +1100

    ipc23 exp hgn test debug

--- learner/hgn/rank_train_eval_hgn.py
+++ learner/hgn/rank_train_eval_hgn.py
@@ -26,9 +26,9 @@ def hgn_rank_evaluate(model, device, val_loader, criterion, return_true_preds=Fa
     model.eval()
     val_loss = 0
 
-    if return_true_preds:
-        y_true = torch.tensor([])
-        y_pred = torch.tensor([])
+    # if return_true_preds:
+    #     y_true = torch.tensor([])
+    #     y_pred = torch.tensor([])
 
     for data in val_loader:
         data = data.to(device)
@@ -39,15 +39,15 @@ def hgn_rank_evaluate(model, device, val_loader, criterion, return_true_preds=Fa
         loss = criterion.forward(h_pred, h_true)
         val_loss += loss.detach().cpu().item()
 
-        if return_true_preds:
-            y_pred = torch.cat((y_pred, h_pred.detach().cpu()))
-            y_true = torch.cat((y_true, h_true.detach().cpu()))
+        # if return_true_preds:
+        #     y_pred = torch.cat((y_pred, h_pred.detach().cpu()))
+        #     y_true = torch.cat((y_true, h_true.detach().cpu()))
 
     stats = {
         "loss": val_loss / len(val_loader),
     }
-    if return_true_preds:
-        stats["y_true"] = y_true
-        stats["y_pred"] = y_pred
+    # if return_true_preds:
+    #     stats["y_true"] = y_true
+    #     stats["y_pred"] = y_pred
 
     return stats
