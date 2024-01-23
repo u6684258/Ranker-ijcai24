487aceb135bce1b972fe17b226dea0079c967c2f
Mingyu Hao <mingyu.hao@anu.edu.au>
2024-01-23 15:28:06 +1100

    clean up codes

--- learner/scripts_gnn/collect_cluster1_test.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p icaps24_test_logs
-ssh cluster1 "cd goose-kernels/learner; tar -czvf icaps24_test_logs.tar.gz icaps24_test_logs"
-scp cluster1:~/goose-kernels/learner/icaps24_test_logs.tar.gz .
-tar -xzvf icaps24_test_logs.tar.gz
-rm icaps24_test_logs.tar.gz
